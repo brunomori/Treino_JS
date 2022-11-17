2d5639a89f1f32e3d80bdcd6d43c365fd0818697
brunomori <brunomanzinimori@Gmail.com>
2022-11-16 23:48:52 -0300

    att

--- for/script.js
+++ for/script.js
@@ -1,24 +1,31 @@
-function contar(){
-     var ini = document.getElementById('txti')
-     var fim = document.getElementById('txtf')
-     var passo=document.getElementById('txtp')
-     var res= document.getElementById('res')
+function contar() {
+   var ini = document.getElementById('txti')
+   var fim = document.getElementById('txtf')
+   var passo = document.getElementById('txtp')
+   var res = document.getElementById('res')
+   res.innerHTML=''
 
-       if(ini.value.length ==0|| fim.value.length==0|| passo.value.length==0){
-        window.alert('[ERRO] Veirique os dados') }
-        else{
-           res.innerHTML='Contando'
-           let i=Number(ini.value)
-           let f=Number(fim.value)
-           let p=Number(passo.value)
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+      window.alert('[ERRO] Veirique os dados')
+   }
+   else {
+
+      let i = Number(ini.value)
+      let f = Number(fim.value)
+      let p = Number(passo.value)
 
 
-            for(let c=i; c<=f ; c+=p)
-            document.getElementById('res').innerHTML+=`${c}`
-           //c=i(c = ao inicio)  //c <=fim // :contador vai receber ele mesmo mais o passo 
-           // C= c+p    
-           //res = o que ele ja tem +  o novo ciclo
+      if (i > f) {
+         for (let c = i; c >= f; c -= p)
+            res.innerHTML += `${c} `
+      }
+      else {
+         for (let c = i; c <= f; c += p)
+            res.innerHTML += `${c} `
+      }
+      //c=i(c = ao inicio)  //c <=fim // :contador vai receber ele mesmo mais o passo 
+      // C= c+p    
+      //res = o que ele ja tem +  o novo ciclo
 
-           }
-        }
-    
+   }
+}
