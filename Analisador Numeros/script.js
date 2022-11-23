3a09cbd4daf8653d15c43d189ecab8dbf7ec9689
brunomori <brunomanzinimori@Gmail.com>
2022-11-22 21:25:39 -0300

    att

---
+++ Analisador Numeros/script.js
@@ -0,0 +1,34 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores= []
+
+
+
+function isNumero(n){
+    if(Number)
+
+}
+
+
+function inLista(n,l){
+
+}
+
+
+
+function adiconar(){
+    if(isNumero(num.value)&& !inLista(num.value,valores)){
+
+    }
+    else{
+        window.alert('Valor invalido ou já encontra na lista')
+    }
+
+
+}
+
+
+function validar(){
+
+}
