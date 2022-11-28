b35f9aee21aa6334114511d534bd977cb6137d3c
brunomori <brunomanzinimori@Gmail.com>
2022-11-28 17:22:25 -0300

    att

---
+++ Lampada/Lampada.js
@@ -0,0 +1,19 @@
+const turnOn=document.getElementById('turnOn');
+const turnOff=document.getElementById('turnOff');
+const lamp=document.getElementById('lamp');
+
+
+function LampOn(){
+    document.getElementById("lamp").src="img/ligada.jpg"
+  
+}
+
+function LampOff(){
+    document.getElementById("lamp").src="img/desligada.jpg"
+}
+
+
+function LampQuebra(){
+    document.getElementById("lamp").src="img/quebrada.jpg"
+
+}
