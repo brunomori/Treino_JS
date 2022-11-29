15cf2f6f36fc7918a6371763dea47014653b0699
brunomori <brunomanzinimori@Gmail.com>
2022-11-29 11:35:09 -0300

    att

--- Calculadora- IMC/script.js
+++ Calculadora- IMC/script.js
@@ -1,20 +1,35 @@
-function tabuada(){
-    let num = document.getElementById('txtn')
-    let tab= document.getElementById('seltab')
-    if ( num.value.length==0){
-        window.alert('ERRO, digite um numero')
-    }
-    else{
-        let = n =Number(num.value)
-        
-        tab.innerHTML =' '
-        for(let c = 1; c<=10; c++)
-        {
-            let item = document.createElement('option')
-            item.text=`${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
 
+function calcular() {
+    const altura = document.getElementById('ialtura').value
+    const peso = document.getElementById('ipeso').value
+    const nome = document.getElementById('inome').value
+    const resultado = document.getElementById('ires')
+
+    if (nome !== '' && peso !== '' && altura !== '') {  //se valor de nome foi diferente de '' campo vazio
+        const valorIMC = (peso / (altura * altura)).toFixed(2) //toFixed(2) Redonda duas casas
+        resultado.textContent = valorIMC
+
+        let classificacao = ""
+        if (valorIMC < 18.5) {
+            classificacao = 'abaixo do peso'
+        } else if (valorIMC < 25) {
+            classificacao = 'peso ideal. Parabens'
+        } else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso'
+        } else if (valorIMC < 35) {
+            classificacao = 'com obesidade grau 1'
+        } else if (valorIMC < 40) {
+            classificacao = 'com obesidade grau 2'
+        } else {
+            classificacao = 'com obesidade grau 3. Cuidado!!!'
         }
-    }
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
 
+    }
+    else {
+        window.alert('ERRO!')
+        resultado.textContent = 'Preencha todosos campos!'
+    }
 }
+
+
