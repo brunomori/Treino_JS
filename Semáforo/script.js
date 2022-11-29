cd5a68725c6e989ad36c0471de1bb49ad1b6731a
brunomori <brunomanzinimori@Gmail.com>
2022-11-29 20:26:49 -0300

    att

---
+++ Semáforo/script.js
@@ -0,0 +1,23 @@
+function vermelho() {
+    document.getElementById('img').src = "img/vermelho.png"
+
+}
+
+function amarelo() {
+    document.getElementById('img').src = "img/amarelo.png"
+
+}
+
+function verde() {
+    document.getElementById('img').src = "img/verde.png"
+
+}
+
+function automatico() {
+    
+    document.getElementById('img').src = "img/vermelho.png"
+    document.getElementById('img').src = "img/amarelo.png"
+    document.getElementById('img').src = "img/verde.png"
+
+}
+
