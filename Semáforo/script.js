48475421d162b3356158b46ae80778d239f3b4fa
brunomori <brunomanzinimori@Gmail.com>
2023-01-16 17:45:29 -0300

    att

--- Semáforo/script.js
+++ Semáforo/script.js
@@ -1,23 +1,54 @@
-function vermelho() {
-    document.getElementById('img').src = "img/vermelho.png"
+const img = document.getElementById("img")
 
+
+function red() {
+    img.src = "img/vermelho.png"
 }
 
-function amarelo() {
-    document.getElementById('img').src = "img/amarelo.png"
-
+function yellow() {
+    img.src = "img/amarelo.png"
 }
 
-function verde() {
-    document.getElementById('img').src = "img/verde.png"
-
+function green() {
+    img.src = "img/verde.png"
 }
 
-function automatico() {
-    
-    document.getElementById('img').src = "img/vermelho.png"
-    document.getElementById('img').src = "img/amarelo.png"
-    document.getElementById('img').src = "img/verde.png"
 
+
+var interval = 0;
+
+function automatic() {
+    interval = setInterval(() => {
+        console.log(interval)
+
+        function red() {
+            img.src = "img/vermelho.png"
+        }
+        setTimeout(red, 2500)
+
+        function yellow() {
+            img.src = "img/amarelo.png"
+        }
+        setTimeout(yellow, 3500)
+
+        function green() {
+            img.src = "img/verde.png"
+        }
+
+
+        setTimeout(green, 4500)
+
+    }, 5000)
+}
+
+function stop() {
+    console.log(interval)
+
+    setTimeout(() => {
+        img.src = 'img/desligado.png'
+        clearInterval(interval)
+        console.log('executei');
+
+    })
 }
 
