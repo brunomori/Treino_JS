0f4c862165642579577f8dac3ec4dcc2c8136598
brunomori <brunomanzinimori@Gmail.com>
2023-02-17 15:43:00 -0300

    att

---
+++ RocketSite/Function/ex2.js
@@ -0,0 +1,14 @@
+// Criar um aplicativo de frases com funciton 
+
+
+// Chame esse aplicativo 3 vezes
+
+// Passar argumentos para a invocação de uma função e criar uma função que vao receber os paremetros da função.
+// criar uma função que vai receber number1 e number2 dentro do seus() , fazer a função somar ambos e printar.
+// criar o valor para função atraves de sum(2,3) separado
+const sum= function(number1,number2){
+    console.log(number1+number2)
+}
+
+sum(2,3)
+
